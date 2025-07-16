1061a204af736bb3e5c33fc5807fac9405500b6f
Matheus Marques <marques.mdom7@gmail.com>
2025-07-16 16:54:34 -0400

    Add route concept

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+//ROTA é constituída por 3 métodos diferentes
+    //Method HTTP criam o acrônimo do CRUD (Create, Read, Update, Delete)
+        //GET: Pega uma informação
+        //POST: Cria uma informação
+        //PUT: Altera toda a informação
+        //PATCH: Altera parte da informação
+        //DELETE: Apaga uma informação
+    
+    
+    //Name: Um identificaor da rota, ex: /home https:google.com
+    //Function: (Callback) - Responsável por executar algum comando
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
