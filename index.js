9881e6ab870239e87aa660598d9f0bbe96c2eba5
Matheus Marques <marques.mdom7@gmail.com>
2025-07-16 15:13:20 -0400

    init MERN STACK

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Exemplo de uso do express chamando a porta: ${port}`)
+})
